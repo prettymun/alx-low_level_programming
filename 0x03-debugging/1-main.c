a2ced96d592bc50190d475df4bf00f5dc161de38
Purity Musambi <avulamusipurity@gmail.com>
2023-07-20 17:22:44 -0700

    3-print_remaining_days.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,9 +8,9 @@
 int main(void)
 {
 int i;
-
-printf("Infinite loop incoming :(\n");
-
+{
+	printf("Infinite loop incoming :(\n");
+}
 i = 0;
 /**
 * while (i < 10)
@@ -18,7 +18,8 @@ i = 0;
 *  putchar(i);
 * }
 */
-printf("Infinite loop avoided! \\o/\n");
-
+{
+	printf("Infinite loop avoided! \\o/\n");
+}
 return (0);
 }
--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -13,9 +13,10 @@ int main(void)
     int day;
     int year;
 
-    month = 4;
-    day = 1;
-    year =1997;
+    month = 2;
+    day = 29;
+    year = 2000;
+
     printf("Date: %02d/%02d/%04d\n", month, day, year);
 
     day = convert_day(month, day);
@@ -24,4 +25,3 @@ int main(void)
 
     return (0);
 }
-

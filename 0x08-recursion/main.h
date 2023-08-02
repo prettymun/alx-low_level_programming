c45c06f706e5dbc9d92fe0384191d525da487934
Purity Musambi <avulamusipurity@gmail.com>
2023-08-02 13:46:44 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,9 +1,6 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
+void _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
-void _putchar(char c);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
@@ -12,5 +9,3 @@ int _sqrt(int n, int i);
 int wildcmp(char *s1, char *s2);
 int is_prime_number(int n);
 int is_palindrome(char *s);
-
-#endif
